c673dabf081ec3eb63d5f5f03445a6fd2a63a3dd
LinLin <131834728+ThAolInh20@users.noreply.github.com>
2025-12-13 15:58:58 +0700

    chuyển sang dùng model 2.5 flash lite

--- app/api/translate/route.js
+++ app/api/translate/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
 
     const apiKey = process.env.GEMINI_API_KEY;
     const url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" +
       apiKey;
 
     let response;
